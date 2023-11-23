a67973b4a594a127f31292037f5dc1b1ff5b5dc1
张扬 <bluesyoung_web@163.com>
2023-11-23 09:55:09 +0800

    feat!: 🚀 广电出手，使用官方直播

--- pages/index.vue
+++ pages/index.vue
@@ -1,40 +1,35 @@
 <!--
  * @Author: zhangyang
  * @Date: 2023-07-21 09:27:14
- * @LastEditTime: 2023-11-22 09:49:06
+ * @LastEditTime: 2023-11-23 09:43:57
  * @Description:
 -->
 <script setup lang="ts">
-import m3u8Raw from '@/assets/global.m3u?raw'
+import m3u8Raw from '@/assets/cctv.m3u?raw'
 
 const rawM3u8 = ref(m3u8Raw)
 
 const showLogo = ref(false)
 
-fetch('https://live.fanmingming.com/tv/m3u/global.m3u').then(r => r.text()).then((raw) => {
-  console.log('远程优先')
-  rawM3u8.value = raw
+fetch('https://live.fanmingming.com/tv/CCTV1.png').then(() => {
+  console.log('显示 logo')
   showLogo.value = true
 }).catch(() => {
-  console.log('远程获取失败，使用本地')
-  rawM3u8.value = m3u8Raw
+  console.log('logo 获取失败')
 })
 
 const validTVs = computed(() => rawM3u8.value.match(/(?<=#EXTINF:-1\s)(.*\n.*)/img)?.filter(tv => tv.includes('tvg-id')) ?? [])
 
-const TVGroups = useLocalStorage('TVGroups', new Map<string, {
+interface TVItem {
   tvgId: string
   tvgName: string
   tvgLogo: string
   src: string
-}[]>())
+}
 
-const TVList = useLocalStorage('TVList', new Set<{
-  tvgId: string
-  tvgName: string
-  tvgLogo: string
-  src: string
-}>())
+const TVGroups = useLocalStorage('TVGroups', new Map<string, TVItem[]>())
+
+const TVList = useLocalStorage('TVList', new Set<TVItem>())
 
 process.client && watch(() => validTVs.value, (tvs) => {
   TVList.value.clear()
@@ -62,13 +57,17 @@ process.client && watch(() => validTVs.value, (tvs) => {
       TVGroups.value.set(groupTitle, [{ tvgId, tvgName, tvgLogo, src }])
   }
 }, { deep: true, immediate: true })
+
+function jumpCCTV(tv: TVItem) {
+  window.open(`https://tv.cctv.com/live/${tv.tvgId.toLowerCase()}`)
+}
 </script>
 
 <template>
   <div v-for="([group, tvs]) of TVGroups.entries()" :key="group">
     <ElCard :header="group" body-style="background: #999;">
       <div class="flex flex-wrap gap-32px">
-        <div v-for="tv of tvs" :key="tv.tvgId" class="flex justify-center items-center cursor-pointer" :title="tv.tvgName" @click="navigateTo(`/play/${encodeURIComponent(tv.src)}`)">
+        <div v-for="tv of tvs" :key="tv.tvgId" class="flex justify-center items-center cursor-pointer" :title="tv.tvgName" @click="jumpCCTV(tv)">
           <img v-if="showLogo" class="w-120px" :src="tv.tvgLogo">
           <ElAvatar v-else class="!w-160px" shape="square">
             <div class="text-2xl">
